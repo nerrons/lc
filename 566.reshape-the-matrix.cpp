6e5461c5dc5c216b297237adfe79ce33c4a70fe2
nerrons <coniinedieu@gmail.com>
2019-10-30 01:07:53 +0800

    566

--- 566.reshape-the-matrix.cpp
+++ 566.reshape-the-matrix.cpp
@@ -68,12 +68,28 @@
  * 
  * 
  */
-
+#include<vector>
+#include<map>
+#include<string>
+#include<iostream>
+using namespace std;
 // @lc code=start
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
-        
+        int m = nums.size(), n = nums[0].size();
+        if (m * n != r * c) return nums;
+
+        vector<vector<int>> result(r, vector<int>(c));
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // seems like a good way to deal with reshaping matrices
+                result[count / c][count % c] = nums[i][j];
+                count++;
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
